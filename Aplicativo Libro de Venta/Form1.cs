e75995079092af4a3f715804be4382982bf65565
Jonathan Cuesto <34cuesto@gmail.com>
2014-07-01 15:46:22 -0400

    se agregó un botón

--- Aplicativo Libro de Venta/Form1.cs
+++ Aplicativo Libro de Venta/Form1.cs
@@ -16,5 +16,10 @@ namespace Aplicativo_Libro_de_Venta
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
